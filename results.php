774f2ccf0f3310fc0eb7d761ef3954b73ee99bde
rashmir <rashmi.raghunandan93@gmail.com>
2014-04-14 18:44:25 +0530

    php

--- results.php
+++ results.php
@@ -2,27 +2,25 @@
 // Config  
 $dbhost = 'localhost';  
 $dbname = 'testco';  
-$name=$_POST["username"];
-$password=$_POST["password"];
+//$name=$_POST["username"];
+//$password=$_POST["password"];
 // Connect to test database  
 $m = new Mongo("mongodb://$dbhost");  
 $db = $m->$dbname;  
   
 // select the collection  
-$collection = $db->logins;  
+$collection = $db->mockresult;  
 //insert a tupple
-$item=array('uname'=>'neo','age'=>28,'profession'=>'hacker');
+//$item=array('uname'=>'neo','age'=>28,'profession'=>'hacker');
 //$collection->insert($item);
   
 // pull a cursor query  
-$aa=array('uname'=>$name);
+$aa=array('uname'=>'manisha','mcqs'=>45);
 $sds=array('_id'=>false);
 $cursor = $collection->find($aa,$sds);
-  foreach($cursor as $document) {
-	$p=array('pass'=>$password);
-	$cursor1 = $collection->find($p,$sds);
-		foreach($cursor1 as $document) {
+  foreach($cursor as $document) 
+  {
 			echo json_encode($document);
-		//	header('Location:profile.html');			
-} } 
+			//header('Location:profile.html');			
+} 
 ?>   
--- test.php
+++ test.php
@@ -2,27 +2,27 @@
 // Config  
 $dbhost = 'localhost';  
 $dbname = 'testco';  
-$name=$_POST["username"];
-$password=$_POST["password"];
+//$name=$_POST["username"];
+//$password=$_POST["password"];
 // Connect to test database  
 $m = new Mongo("mongodb://$dbhost");  
 $db = $m->$dbname;  
   
 // select the collection  
-$collection = $db->logins;  
+$collection = $db->mcq;  
 //insert a tupple
-$item=array('uname'=>'neo','age'=>28,'profession'=>'hacker');
+//$item=array('uname'=>'neo','age'=>28,'profession'=>'hacker');
 //$collection->insert($item);
   
 // pull a cursor query  
-$aa=array('uname'=>$name);
-$sds=array('_id'=>false);
+$aa=array('sub'=>'phy');
+$sds=array('ca'=>false);
 $cursor = $collection->find($aa,$sds);
   foreach($cursor as $document) {
-	$p=array('pass'=>$password);
+	$p=array('tag'=>'gravity');
 	$cursor1 = $collection->find($p,$sds);
 		foreach($cursor1 as $document) {
 			echo json_encode($document);
-		//	header('Location:profile.html');			
+			//header('Location:profile.html');			
 } } 
 ?>   
