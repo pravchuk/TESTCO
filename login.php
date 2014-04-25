80bc437e9708f910c20368a62d62c4223e4a15b6
pravchuk <pravchuk@gmail.com>
2014-04-25 19:40:20 +0530

    part of the session authentication

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ $dbname = 'testco';
 
 $name=$_POST["username"];
 $password=$_POST["password"];
+$url = $_POST["url"];
 // Connect to test database  
 $m = new Mongo("mongodb://$dbhost");  
 $db = $m->$dbname;  
@@ -17,18 +18,38 @@ $aa=array('uname'=>$name);
 $sds=array('_id'=>false);
 $cursor = $collection->find($aa,$sds);
   foreach($cursor as $document) {
-	$p=array('pass'=>$password,'uname'=>$name);
-	$cursor1 = $collection->find($p,$sds);
+
+  	//checking if cookie exists
+  	if($_COOKIE['sessionid']){
+  		$d = array('sessionid'=>$_COOKIE['sessionid']);
+  		$cursor1 = $collection->find($d,$sds);
+  		if($cursor1){
+  			echo "Authenticated";
+  		}else{
+  			//go to login page
+  			header('Location:login.php');
+  		}
+  	}
+  	else{
+		$p=array('pass'=>$password,'uname'=>$name);
+		$cursor1 = $collection->find($p,$sds);
+
 		foreach($cursor1 as $doc) {
 			echo json_encode($doc);
-			$url="profiles.php?username=".$name;
-			header('Location:profiles.php?username='.$name);
 		
 			$mapassword = 'chukkaisgreat';
 			$mysession = md5($username.$password.$mapassword);
-			setCookie('dasession',$mysession);
+			setcookie('sessionid',$mysession);
 			
-			$addDemSession = $collection->update$mysession;
+			$newdata = array('$set' => array("sessionid" => $mysession));
+			$collection->update(array("uname" => $username), $newdata);
+
+			// $url="profiles.php?username=".$name;
+
+			// header('Location:profiles.php?username='.$name);
 			
-} } 
+		}
+	}
+
+} 
 ?>   
